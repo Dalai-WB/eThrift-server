b4fd16000bba5fb627fe8eead964e6086f297ffd
Temuulen996 <temuulenuuree@gmail.com>
2023-04-29 15:31:46 +0800

    t

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
   fname: { type: String, required: true },
   lname: { type: String, required: true },
-  picture: { type: [String], require: true },
+  picture: { type: String, require: true },
   phone_number: { type: Number, required: true },
   email: { type: String, required: true },
 });
